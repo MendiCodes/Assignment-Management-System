78b9d6306adb9cfd860167c0cc264ff76c35107d
MendiCodes <zodakyt29@gmail.com>
2026-01-07 14:54:26 +0100

    Update backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,9 @@ const path = require("path");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 
 app.use("/api/assignments", assignmentRoutes);
